a0e026d4ba40adf54344d5326b3c9f919d39291e
Prince22i3 <sarangiprinceraj22i3@gmail.com>
2026-09-04 17:37:35 +0530

    shell v1.0

--- src/main.c
+++ src/main.c
@@ -2,26 +2,74 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 
-char *buffer = NULL;
-size_t buff_size = 0;
-ssize_t text;
 
 int main(){
     system("clear");
-    printf("eternity-shell $ ");
 
-    text = getline(&buffer, &buff_size, stdin);
+    while(1){
+        printf("eternity-shell $ ");
 
-    if(text == -1){
-        perror("Error reading input or EOF reached.\n");
+        char *buffer = NULL;
+        size_t buff_size = 0;
+        ssize_t text;
+
+        text = getline(&buffer, &buff_size, stdin);
+
+        if(text == -1){
+            if(feof(stdin)){
+                printf("EOF detected. Exiting eternity-shell...\n");
+                free(buffer);
+                exit(EXIT_SUCCESS);
+            }
+            if(ferror(stdin)){
+                perror("eternity-shell: Critical read error");
+                free(buffer);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        if(text>0 && buffer[text-1] == '\n') buffer[text-1] = '\0';
+        if(buffer[0]=='\0'){
+            free(buffer);
+            continue;
+        }
+    
+        char *args[text+1];
+        int i = 0;
+
+        char *token = strtok(buffer, " ");
+        while(token != NULL && i<text){
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+        pid_t id = fork();
+        if(id < 0){
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+        }
+        else if(id == 0){
+            execvp(args[0], args);
+            perror("Exec failed");
+            exit(EXIT_FAILURE);
+        }
+        else{
+            int status;
+            waitpid(id, &status, 0);
+
+            if(WIFEXITED(status)){
+                printf("Child exited cleanly with status: %d\n", WEXITSTATUS(status));
+            }
+            else if(WIFSIGNALED(status)){
+                printf("Child killed violently by a signal\n");
+            }
+        }
+
         free(buffer);
-        return 1;
     }
 
-    if(text>0 && buffer[text-1] == '\n') buffer[text-1] = '\0';
-
-    printf("%s", buffer);
-    free(buffer);
     return 0;
 }
