2c28ecdc1720fef3904a68c91701363ac9279a7c
Prince22i3 <sarangiprinceraj22i3@gmail.com>
2026-09-16 23:25:50 +0530

    split main to modular functions

--- src/main.c
+++ src/main.c
@@ -22,10 +22,261 @@ void handle_sigchld(int sig){
         int res = waitpid(background_pids[i], &status, WNOHANG);
         if(res > 0){   
             printf("\nThe child process with pid, [%d], has finished executing\n", background_pids[i]);
+            printf("eternity-shell $ ");
+            fflush(stdout);
             shift_back(&background_pids[i]);
             bid--;
             i--;
         }
+    }
+}
+
+
+char *read_input(ssize_t *text){
+    char *buffer = NULL;
+    size_t buff_size = 0;
+    *text = getline(&buffer, &buff_size, stdin);
+
+    if(*text == -1){
+        if(feof(stdin)){
+            printf("EOF detected. Exiting eternity-shell...\n");
+            free(buffer);
+            exit(EXIT_SUCCESS);
+        }
+        if(ferror(stdin)){
+            perror("eternity-shell: Critical read error");
+            free(buffer);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if(*text>0 && buffer[*text-1] == '\n') buffer[*text-1] = '\0';
+
+    return buffer;
+}
+
+
+char trunc_io[2] = ">";
+char input[2] = "<";
+char append[3] = ">>";
+char pipe_ch[2] = "|";
+char ampersand[2] = "&";
+
+void tokenise(char *args[], int *i, char *buffer){
+    char *sp_ch_ptr = NULL;
+
+    char *src = buffer;
+    char *dest = buffer;
+        
+    while(*src != '\0'){
+        
+        while(*src == ' '){
+            src++;
+        }
+        if(*src == '\0') break;
+        dest = src;
+        
+        if(*dest == '"' && *(dest+1) == '"'){
+            dest++;
+            src++;
+        }
+        if(*dest != '&') args[(*i)++] = dest;
+        
+        int in_quotes = 0;
+        
+        while(*src != '\0'){
+            
+            if(*src == '"'){
+                in_quotes = !in_quotes;
+                src++;
+            }
+            if(!in_quotes && *src == ' '){
+                break;
+            }
+            if(!in_quotes && (*src == '<' || *src == '>' || *src == '|' || *src == '&')){
+                if(*src == '>' && *(src+1)=='>'){
+                    sp_ch_ptr = append;
+                    *(src+1) = ' ';
+                }
+                else if(*src == '>'){
+                    sp_ch_ptr = trunc_io;
+                }
+                else if(*src == '<'){
+                    sp_ch_ptr = input;
+                }
+                else if(*src == '|'){
+                    sp_ch_ptr = pipe_ch;
+                }
+                else if(*src == '&'){
+                    sp_ch_ptr = ampersand;
+                }
+                args[(*i)++] = sp_ch_ptr;
+                break;
+            }
+            
+            if(*src != '"') *dest = *src;
+            dest++;
+            src++;
+        }
+        
+        *dest = '\0';
+        src++;
+    }
+    
+    args[*i] = NULL;
+}
+
+void parse(
+    char *args[], 
+    int *background_process,
+    char *commands[10][10],
+    char* operation[],
+    char* file[],
+    int redirect_command_index[],
+    int *j,
+    int *r,
+    int *c,
+    int *o,
+    int *current_command
+    ){
+    while(args[*j] != NULL){
+        if(strcmp(args[*j],"&") == 0){
+            *background_process = 1;
+            args[*j] = NULL;
+            break;
+        }
+        if(strcmp(args[*j],"|") == 0){
+            commands[*r][*c] = NULL;
+            (*r)++;
+            *c=0;
+            (*j)++;
+            (*current_command)++;
+            continue;
+        }
+        if(strcmp(args[*j],">")==0 || strcmp(args[*j],">>")==0 || strcmp(args[*j],"<")==0){
+            operation[*o] = args[*j];
+            file[*o] = args[*j+1];
+            redirect_command_index[*o] = *current_command;
+            (*o)++;
+            (*j) += 2;
+            continue;
+        }
+        commands[*r][(*c)++] = args[*j];
+        (*j)++;
+    }
+    commands[*r][*c] = NULL;
+}
+
+void execute_commands(
+    char *args[],
+    int *cnt,
+    int *prev_pipe,
+    int foreground_pids[10],
+    int *fid,
+    int *r,
+    int *o,
+    int *background_process,
+    int redirect_command_index[],
+    char* operation[],
+    char* file[],
+    char *commands[10][10]
+){
+    for(*cnt = 0; *cnt<=*r; (*cnt)++){
+            
+        int fd[2];
+        if(*cnt<*r) {
+            pipe(fd);
+        }
+
+        // fork syetem call
+        pid_t id = fork();
+
+        if(id < 0){
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+        }
+        // Child Process
+        else if(id == 0){
+            if(*prev_pipe != -1){
+                dup2(*prev_pipe,0);
+                close(*prev_pipe);
+            }
+            if(*cnt<*r){
+                close(fd[0]);
+                dup2(fd[1],1);
+                close(fd[1]);
+                }
+    
+
+            // I/O Redirection
+            for(int x=0; x<*o; x++){
+                if(redirect_command_index[x]!=*cnt) continue;
+                if(strcmp(operation[x],">")==0){
+                    int file_fd = open(file[x], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    if(file_fd == -1){
+                        fprintf(stderr, "eternity-shell: %s ", file[x]);
+                        perror("");
+                        _exit(EXIT_FAILURE);
+                    }
+                    if(dup2(file_fd, 1) < 0){
+                        perror("eternity-shell: redirection failed");
+                        close(file_fd);
+                        _exit(EXIT_FAILURE);
+                    }
+                    close(file_fd);
+                }
+                else if(strcmp(operation[x],">>")==0){
+                    int file_fd = open(file[x], O_WRONLY | O_CREAT | O_APPEND, 0644);
+                    if(file_fd == -1){
+                        fprintf(stderr, "eternity-shell: %s ", file[x]);
+                        perror("");
+                        _exit(EXIT_FAILURE);
+                    }
+                    if(dup2(file_fd, 1) < 0){
+                        perror("eternity-shell: redirection failed");
+                        close(file_fd);
+                        _exit(EXIT_FAILURE);
+                    }
+                    close(file_fd);
+                }
+                else{
+                    int file_fd = open(file[x], O_RDONLY, 0644);
+                    if(file_fd == -1){
+                        perror("File does not exist");
+                        _exit(EXIT_FAILURE);
+                    }
+                    if(dup2(file_fd, 0) < 0){
+                        perror("eternity-shell: redirection failed");
+                        close(file_fd);
+                        _exit(EXIT_FAILURE);
+                    }
+                    close(file_fd);
+                }
+            }
+            execvp(commands[*cnt][0], commands[*cnt]);
+            // exec system call
+            execvp(args[0], args);
+            perror("Exec failed");
+            _exit(EXIT_FAILURE);
+        }
+        //Parent Process
+        else{                
+            if(*background_process) 
+                background_pids[bid++] = id;
+            else 
+                foreground_pids[(*fid)++] = id;
+            if(*prev_pipe != -1){
+                close(*prev_pipe);
+            }
+            if(*cnt<*r){
+                close(fd[1]);
+                *prev_pipe = fd[0];
+            }
+        }
+    }
+    while(*fid>0){
+        int status;
+        waitpid(foreground_pids[--(*fid)], &status, 0);
     }
 }
 
@@ -36,105 +287,22 @@ int main(){
     while(1){
         printf("eternity-shell $ ");
 
-        // getline variables
-        char *buffer = NULL;
-        size_t buff_size = 0;
+        //input 
         ssize_t text;
+        char *buffer = read_input(&text);
 
-        text = getline(&buffer, &buff_size, stdin);
-
-        if(text == -1){
-            if(feof(stdin)){
-                printf("EOF detected. Exiting eternity-shell...\n");
-                free(buffer);
-                exit(EXIT_SUCCESS);
-            }
-            if(ferror(stdin)){
-                perror("eternity-shell: Critical read error");
-                free(buffer);
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        if(text>0 && buffer[text-1] == '\n') buffer[text-1] = '\0';
         if(buffer[0]=='\0'){
             free(buffer);
             continue;
         }
     
-        //exec argument vector
+        //tokenise
         char *args[text+1];
         int i = 0;
+        tokenise(args, &i, buffer);
 
-        //Tokenisation
-        char truncate[2] = ">";
-        char input[2] = "<";
-        char append[3] = ">>";
-        char pipe_ch[2] = "|";
-        char ampersand[2] = "&";
-        
-        char *sp_ch_ptr = NULL;
 
-        char *src = buffer;
-        char *dest = buffer;
-        
-        while(*src != '\0'){
-            
-            while(*src == ' '){
-                src++;
-            }
-            if(*src == '\0') break;
-            dest = src;
-            
-            if(*dest == '"' && *(dest+1) == '"'){
-                dest++;
-                src++;
-            }
-            if(*dest != '&') args[i++] = dest;
-            
-            int in_quotes = 0;
-            
-            while(*src != '\0'){
-                
-                if(*src == '"'){
-                    in_quotes = !in_quotes;
-                    src++;
-                }
-                if(!in_quotes && *src == ' '){
-                    break;
-                }
-                if(!in_quotes && (*src == '<' || *src == '>' || *src == '|' || *src == '&')){
-                    if(*src == '>' && *(src+1)=='>'){
-                        sp_ch_ptr = append;
-                        *(src+1) = ' ';
-                    }
-                    else if(*src == '>'){
-                        sp_ch_ptr = truncate;
-                    }
-                    else if(*src == '<'){
-                        sp_ch_ptr = input;
-                    }
-                    else if(*src == '|'){
-                        sp_ch_ptr = pipe_ch;
-                    }
-                    else if(*src == '&'){
-                        sp_ch_ptr = ampersand;
-                    }
-                    args[i++] = sp_ch_ptr;
-                    break;
-                }
-                
-                if(*src != '"') *dest = *src;
-                dest++;
-                src++;
-            }
-            
-            *dest = '\0';
-            src++;
-        }
-        
-        args[i] = NULL;
-
+        //Parsing
         //background processes
         int background_process = 0;
 
@@ -152,32 +320,8 @@ int main(){
         int j = 0;
         int current_command = 0;
 
-        while(args[j] != NULL){
-            if(strcmp(args[j],"&") == 0){
-                background_process = 1;
-                args[j] = NULL;
-                break;
-            }
-            if(strcmp(args[j],"|") == 0){
-                commands[r][c] = NULL;
-                r++;
-                c=0;
-                j++;
-                current_command++;
-                continue;
-            }
-            if(strcmp(args[j],">")==0 || strcmp(args[j],">>")==0 || strcmp(args[j],"<")==0){
-                operation[o] = args[j];
-                file[o] = args[j+1];
-                redirect_command_index[o] = current_command;
-                o++;
-                j += 2;
-                continue;
-            }
-            commands[r][c++] = args[j];
-            j++;
-        }
-        commands[r][c] = NULL;
+        parse(args, &background_process, commands, operation, file, redirect_command_index, &j, &r, &c, &o, &current_command);
+
 
 
         //Exit
@@ -202,110 +346,22 @@ int main(){
         //foreground pids
         int foreground_pids[10];
         int fid = 0;
+        
+        execute_commands(
+            args, 
+            &cnt, 
+            &prev_pipe, 
+            foreground_pids, 
+            &fid, 
+            &r, 
+            &o, 
+            &background_process, 
+            redirect_command_index, 
+            operation, 
+            file, 
+            commands
+        );
 
-        for(cnt = 0; cnt<=r; cnt++){
-            
-            int fd[2];
-            if(cnt<r) {
-                pipe(fd);
-            }
-
-            // fork syetem call
-            pid_t id = fork();
-
-            if(id < 0){
-                perror("Fork failed");
-                exit(EXIT_FAILURE);
-            }
-            // Child Process
-            else if(id == 0){
-
-                if(prev_pipe != -1){
-                    dup2(prev_pipe,0);
-                    close(prev_pipe);
-                }
-                if(cnt<r){
-                    close(fd[0]);
-                    dup2(fd[1],1);
-                    close(fd[1]);
-                }
-    
-
-                // I/O Redirection
-                for(int x=0; x<o; x++){
-                    if(redirect_command_index[x]!=cnt) continue;
-
-                    if(strcmp(operation[x],">")==0){
-                        int file_fd = open(file[x], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                        if(file_fd == -1){
-                            fprintf(stderr, "eternity-shell: %s ", file[x]);
-                            perror("");
-                            _exit(EXIT_FAILURE);
-                        }
-                        if(dup2(file_fd, 1) < 0){
-                            perror("eternity-shell: redirection failed");
-                            close(file_fd);
-                            _exit(EXIT_FAILURE);
-                        }
-                        close(file_fd);
-                    }
-                    else if(strcmp(operation[x],">>")==0){
-                        int file_fd = open(file[x], O_WRONLY | O_CREAT | O_APPEND, 0644);
-                        if(file_fd == -1){
-                            fprintf(stderr, "eternity-shell: %s ", file[x]);
-                            perror("");
-                            _exit(EXIT_FAILURE);
-                        }
-                        if(dup2(file_fd, 1) < 0){
-                            perror("eternity-shell: redirection failed");
-                            close(file_fd);
-                            _exit(EXIT_FAILURE);
-                        }
-                        close(file_fd);
-                    }
-                    else{
-                        int file_fd = open(file[x], O_RDONLY, 0644);
-                        if(file_fd == -1){
-                            perror("File does not exist");
-                            _exit(EXIT_FAILURE);
-                        }
-                        if(dup2(file_fd, 0) < 0){
-                            perror("eternity-shell: redirection failed");
-                            close(file_fd);
-                            _exit(EXIT_FAILURE);
-                        }
-                        close(file_fd);
-                    }
-                }
-
-                execvp(commands[cnt][0], commands[cnt]);
-                // exec system call
-                execvp(args[0], args);
-                perror("Exec failed");
-                _exit(EXIT_FAILURE);
-            }
-
-            //Parent Process
-            else{                
-                if(background_process) 
-                    background_pids[bid++] = id;
-                else 
-                    foreground_pids[fid++] = id;
-
-                if(prev_pipe != -1){
-                    close(prev_pipe);
-                }
-                if(cnt<r){
-                    close(fd[1]);
-                    prev_pipe = fd[0];
-                }
-            }
-        }
-
-        while(fid>0){
-            int status;
-            waitpid(foreground_pids[--fid], &status, 0);
-        }
 
         free(buffer);
     }
